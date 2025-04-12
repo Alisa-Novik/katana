7520ca1f51005c8b2f6151f904df28a71ae96419
Alice Norwood <willingtodev@gmail.com>
2025-04-11 22:50:50 -0400

    bean registry

--- Todo.md
+++ Todo.md
@@ -3,9 +3,17 @@ Katana spec:
 microframework with lightweight http server
 
 - lightweight http server
-Likely connection pool of http connections
+Likely connection pool of http connections.
+
+
 - routing
+Default routes initialization is done
+
+
 - DI framework
+Basic Container and intizitialization already present. Now need to add something like autowiring.
+Maybe explicit mapping/configuration
+
 - error handling
 
 packages:
--- src/main/java/com/norwood/core/KatanaCore.java
+++ src/main/java/com/norwood/core/KatanaCore.java
@@ -1,12 +1,16 @@
 package com.norwood.core;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import com.norwood.networking.KatanaServer;
 import com.norwood.routing.Router;
-import com.norwood.util.KatanaClient;
+import com.norwood.userland.BeanRegistry;
 
 public class KatanaCore
 {
     private static final Container container = new KatanaContainer();
+    public static final Set<Object> beanRegistry = new HashSet<>();
 
     public void boot() {
         System.out.println("Booting Katana");
@@ -18,7 +22,10 @@ public class KatanaCore
         } catch (ContainerException e) {
             throw new RuntimeException("Error initializing container exception.");
         }
-    }
+
+        BeanRegistry.init();
+        processAutowiring();
+        }
 
     private void registerMainBeans() throws ContainerException {
         Container container = getContainer();
@@ -26,6 +33,12 @@ public class KatanaCore
         Router router = new Router();
         router.defineDefaultRoutes();
         container.set(Router.class, router);
+
+        processAutowiring();
+    }
+
+    private void processAutowiring() {
+        beanRegistry.forEach(System.out::println);
     }
 
     public static Container getContainer() {
