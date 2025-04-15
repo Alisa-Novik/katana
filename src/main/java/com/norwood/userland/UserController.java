806c7ad2e3841b73471d134c3eb4fb8400eebc0c
Alice Norwood <willingtodev@gmail.com>
2025-04-14 22:25:24 -0400

    logging 2

--- src/main/java/com/norwood/userland/UserController.java
+++ src/main/java/com/norwood/userland/UserController.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.http.HttpRequest;
 import java.nio.file.Files;
 import java.nio.file.Path;
-
 import com.norwood.core.Singleton;
 import com.norwood.core.annotations.Inject;
 import com.norwood.routing.annotation.Get;
